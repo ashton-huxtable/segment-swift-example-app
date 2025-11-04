020e8faef05c4ed3c93b60e5603effb5365c0c1f
Ashton Huxtable <ashton.huxtable@segment.com>
2025-11-04 11:40:01 -0700

    Initial Commit

---
+++ SwiftExampleApp2/SwiftExampleApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExampleApp2App.swift
+//  SwiftExampleApp2
+//
+//  Created by Ashton Huxtable on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExampleApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
